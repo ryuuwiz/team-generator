b6c0de48ddf5552d4d8f3bf4ff6a697f83d93d42
ryuuwiz <ryuuwiz@gmail.com>
2022-07-13 17:06:06 +0700

    refactor: data-theme attributes

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,9 +4,11 @@ import Layout from "../components/Layout";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
+    <div data-theme="dark">
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </div>
   );
 }
 
