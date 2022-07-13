eb650b89e9d47b06613b666e5146b06ef682b5f2
ryuuwiz <ryuuwiz@gmail.com>
2022-07-13 16:48:53 +0700

    fix: jsx syntax

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -19,7 +19,7 @@ export default function Layout({ children }: Props): JSX.Element {
       <Head>
         <title>Team Generator</title>
         <meta charSet="UTF-8" />
-        <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+        <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <meta name="description" content="Randomly generate teams" />
         <link
